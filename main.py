dd8c142a6e1eb187510db1cca94f9efea40ebb37
Artem Khaydukov <komapartem@gmail.com>
2024-01-27 06:28:19 +0300

    Health Bar, Weapon Bar and Heal Potion

--- main.py
+++ main.py
@@ -11,24 +11,40 @@ pygame.display.set_icon(icon)
 bg = pygame.image.load('images/bg3.png').convert_alpha()
 walk_right = [
     pygame.image.load('images/ghost/walk/right/1.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/right/1 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/right/2.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/right/2 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/right/3.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/right/3 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/right/4.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/right/4 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/right/5.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/right/5 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/right/6.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/right/6 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/right/7.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/right/7 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/right/8.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/right/8 — копия.png').convert_alpha()
 ]
 
 walk_left = [
     pygame.image.load('images/ghost/walk/left/1.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/left/1 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/left/2.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/left/2 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/left/3.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/left/3 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/left/4.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/left/4 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/left/5.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/left/5 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/left/6.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/left/6 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/left/7.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/left/7 — копия.png').convert_alpha(),
     pygame.image.load('images/ghost/walk/left/8.png').convert_alpha(),
+    pygame.image.load('images/ghost/walk/left/8 — копия.png').convert_alpha()
 ]
 
 ghost_step = 0
@@ -53,14 +69,40 @@ enemy_list = []
 
 label = pygame.font.Font('fonts/RubikBurned-Regular.ttf', 80)
 lose_label = label.render('Bro, you lose...', False, 'White')
-restart_label = label.render('Click here to restart', False, (102, 157, 175))
+restart_label = label.render('Click HERE to restart', False, (102, 157, 175))
 restart_label_hitbox = restart_label.get_rect(topleft = (185, 400))
 
-knife = pygame.image.load('images/knife.png').convert_alpha()
+knifes_anim = [
+    pygame.image.load('images/knife/knife1.png').convert_alpha(),
+    pygame.image.load('images/knife/knife1 — копия.png').convert_alpha(),
+    pygame.image.load('images/knife/knife2.png').convert_alpha(),
+    pygame.image.load('images/knife/knife2 — копия.png').convert_alpha(),
+    pygame.image.load('images/knife/knife3.png').convert_alpha(),
+    pygame.image.load('images/knife/knife3 — копия.png').convert_alpha(),
+    pygame.image.load('images/knife/knife4.png').convert_alpha(),
+    pygame.image.load('images/knife/knife4 — копия.png').convert_alpha(),
+]
 knifes = []
 
-knifes_left = 5
-label_knifes = pygame.font.Font('fonts/RubikBurned-Regular.ttf', 30)
+knifes_step = 0
+knifes_left = 3
+
+knife_bar_full = pygame.image.load('images/knife_bar/knife_bar_full.png').convert_alpha()
+knife_bar_medium = pygame.image.load('images/knife_bar/knife_bar_medium.png').convert_alpha()
+knife_bar_low = pygame.image.load('images/knife_bar/knife_bar_low.png').convert_alpha()
+knife_bar_zero = pygame.image.load('images/knife_bar/knife_bar_zero.png').convert_alpha()
+
+hp_full = pygame.image.load('images/hp/hp_full.png').convert_alpha()
+hp_medium = pygame.image.load('images/hp/hp_medium.png').convert_alpha()
+hp_low = pygame.image.load('images/hp/hp_low.png').convert_alpha()
+
+health_points = 3
+
+heal_potion = pygame.image.load('images/potions/medical.png').convert_alpha()
+heal_potion_empty = pygame.image.load('images/potions/medical_empty.png').convert_alpha()
+
+potion_flag = False
+is_potion = False
 
 gameplay = True
 
@@ -69,10 +111,18 @@ while running:
 
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 1280, 0))
-    knifes_left_label = label_knifes.render(f'Knifes left: {knifes_left}', False, 'White')
-    screen.blit(knifes_left_label, (20, 700))
 
     if gameplay:
+
+        if health_points == 3:
+            screen.blit(hp_full, (20, 700))
+
+        if knifes_left == 3:
+            screen.blit(knife_bar_full, (365, 700))
+
+        if is_potion == False:
+            screen.blit(heal_potion_empty, (485, 680))
+
         ghost_hitbox = walk_left[0].get_rect(topleft = (ghost_x, ghost_y))
 
         if enemy_list:
@@ -83,8 +133,27 @@ while running:
                 if el.x < -10:
                     enemy_list.pop(i)
 
-            if ghost_hitbox.colliderect(el):
-                gameplay = False
+                if ghost_hitbox.colliderect(el):
+                    health_points -= 1
+                    enemy_list.pop(i)
+
+        if health_points == 2:
+            screen.blit(hp_medium, (20, 700))
+
+        if health_points == 1:
+            screen.blit(hp_low, (20, 700))
+
+        if health_points == 0:
+            gameplay = False
+
+        if knifes_left == 2:
+            screen.blit(knife_bar_medium, (365, 700))
+
+        if knifes_left == 1:
+            screen.blit(knife_bar_low, (365, 700))
+
+        if knifes_left == 0:
+            screen.blit(knife_bar_zero, (365, 700))
 
         keys = pygame.key.get_pressed()
 
@@ -112,18 +181,23 @@ while running:
                 is_jump = False
                 jump_count = 8
 
-        if ghost_step == 3:
+        if ghost_step == 15:
             ghost_step = 0
         else:
             ghost_step += 1
 
+        if knifes_step == 7:
+            knifes_step = 0
+        else:
+            knifes_step += 1
+
         bg_x -= 2
-        if bg_x == 1280:
+        if bg_x == -1280:
             bg_x = 0
 
         if knifes:
             for i, el in enumerate(knifes):
-                screen.blit(knife, (el.x, el.y))
+                screen.blit(knifes_anim[knifes_step], (el.x, el.y))
                 el.x += 25
 
             if el.x > 1300:
@@ -134,8 +208,28 @@ while running:
                     if el.colliderect(enemy_el):
                         enemy_list.pop(index)
                         knifes.pop(i)
+                        potion_flag = True
+                        potion_x = el.x
+                        potion_y = el.y
+
+        if potion_flag:
+            screen.blit(heal_potion, (potion_x + 5, potion_y + 3))
+            heal_potion_hitbox = heal_potion.get_rect(topleft = (potion_x + 5, potion_y + 3))
+            if heal_potion_hitbox.colliderect(ghost_hitbox):
+                is_potion = True
+                heal_potion_hitbox = heal_potion.get_rect(topleft=(-50, -50))
+                potion_x -= 1500
+                potion_y -= 1500
+
+        if is_potion:
+            screen.blit(heal_potion, (485, 680))
+
+        if keys[pygame.K_e] and is_potion and 0 < health_points < 3:
+            health_points += 1
+            is_potion = False
 
     else:
+        bg_sound.stop()
         screen.fill((87, 88, 89))
         screen.blit(lose_label, (370, 300))
         screen.blit(restart_label, restart_label_hitbox)
@@ -143,13 +237,19 @@ while running:
         mouse = pygame.mouse.get_pos()
         if restart_label_hitbox.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
             gameplay = True
+            bg_sound.play(-1)
             ghost_x = 150
             enemy_list.clear()
             knifes.clear()
-            knifes_left = 5
+            knifes_left = 3
+            bg_x = 0
+            health_points = 3
+            screen.blit(hp_full, (20, 700))
+            screen.blit(knife_bar_full, (365, 700))
+            screen.blit(heal_potion_empty, (485, 680))
+            is_potion = False
 
     pygame.display.update()
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -158,7 +258,7 @@ while running:
         if event.type == enemy_timer:
             enemy_list.append(enemy.get_rect(topleft = (1290, 540)))
         if gameplay and event.type == pygame.KEYUP and event.key == pygame.K_SPACE and knifes_left > 0:
-            knifes.append(knife.get_rect(topleft = (ghost_x + 30, ghost_y + 10)))
+            knifes.append(knifes_anim[0].get_rect(topleft = (ghost_x + 30, ghost_y + 10)))
             knifes_left -= 1
 
-    clock.tick(10)
+    clock.tick(20)
